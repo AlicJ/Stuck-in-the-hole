5f2031ce9225fca9b4da3f1f0a91c744deff1aa0
OpenShift guest <zichen.jiang@yahoo.com>
2013-03-21 09:30:34 -0400

    nohthing

--- script.js
+++ script.js
@@ -1,52 +1,71 @@
-function questionMaker()
-{
-    var num1 = Math.round(20*Math.random());
-	var num2 = Math.round(20*Math.random());
-	if (Math.max(num1, num2)== num2)
-	{
+    var range = 20;
+    var num1 = Math.floor(Math.random()*range)+1;
+	var num2 = Math.floor(Math.random()*range)+1;
+	var operation = Math.floor(Math.random()*4)+1;
+	var answer
+    
+function QuestionMaker(){
+	if (Math.max(num1, num2)== num2){
 		var switcher = num2;
 		num2=num1;
 		num1=switcher;
 	}
-	var operation = Math.round(4*Math.random()+1);
-	var answer;
 	
-	if(operation==1) 
-	{
-		answer = num1+num2;
-		return num1 + " + " + num2 + " = " + answer;
-	}
-	else if(operation==2)
-	{
-		answer = num1-num2;
-		return num1 + " - " + num2 + " = " + answer;
-	}
-	else if(operation>=3)
-	{
+	if(operation>=3){
 		num1 = halver(num1+1);
 		num2 = halver(num2+1);
-		if(operation ==3)
-		{
-			answer = num1*num2;
-			return num1 + " * " + num2 + " = " + answer;
-		}
-		else
-		{
+	}
+	
+	switch(operation){
+		case 1: //addition
+			answer = num1 + num2;
+			$("#question").append("what is " + num1 + " plus " + num2+ "?");
+			break;
+		case 2: //subtraction
+			answer = num1 - num2;
+			$("#question").append("what is " + num1 + " minus " + num2+ "?");
+			break;
+		case 3: //multiplication
+			answer = num1 * num2;
+			$("#question").append("what is " + num1 + " multiplied by " + num2+ "?");
+			break;
+		case 4: //division
 			answer = num1;
-			num1 *=num2;
-			return num1 + " / " + num2 + " = " + answer;
-		}
-}
-}
-function halver (number)
-{
-	if(number>10)
-	{
+			num1 *= num2;
+			answer = num1 / num2;
+			$("#question").append("what is " + num1 + " divided by " + num2+ "?");
+			break;
+	}
+}	
+	
+	$(document).ready(function() {
+        $("#submit").click(function(){
+			var value = $("#value").val();
+			//alert(answer);
+			//confirm(value);
+			if(value == answer){
+				$("#result").text("You are correct.");	
+			}else{
+				$("#result").text("You are wrong.");
+			}
+		})
+    });
+	
+	function halver (number){
+	if(number>10){
 		number /=2;
 	}
-	return Math.round(number);
-}
-function aaaaaaa()
-{
-	$("#output").text(questionMaker);
-}
+	return Math.floor(number);
+	}
+	
+		
+	$(document).ready(function(){
+		$("#container").append("<input type=\"submit\" id=\"" + num1 + "\" value=\"" + num1 + "\"/>");
+		$("#container").append("<input type=\"submit\" id=\"" + num2 + "\" value=\"" + num2 + "\"/>");
+		$('#'+num1).click(function(){
+			$("#container").append("<div>"+num1+"</div>");
+		});
+		$('#'+num2).click(function(){
+			$("#container").append("<div>"+num2+"</div>");
+		});
+	});
