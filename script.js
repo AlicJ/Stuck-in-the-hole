83105183e1ae0b597a752ab00c2d2da4da233dcf
OpenShift guest <zichen.jiang@yahoo.com>
2013-04-05 09:02:20 -0400

    update all the changes this week

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Question Maker
     var range = 20;
-	var num1 = Math.floor(Math.random()*range)+1;
+    var num1 = Math.floor(Math.random()*range)+1;
 	var num2 = Math.floor(Math.random()*range)+1;
 	var operation = Math.floor(Math.random()*4)+1;
 	var answer;
@@ -21,25 +21,25 @@
 		switch(operation){
 			case 1: //addition
 				answer = num1 + num2;
-				$("#question").append("What is " + num1 + " plus " + num2+ "? ");
-				question = "What is " + num1 + " plus " + num2+ "? ";
+				$("#question").text("What is " + num1 + " plus " + num2+ "? ");
+				question = num1 + " + " + num2+ " = ? ";
 				break;
 			case 2: //subtraction
 				answer = num1 - num2;
-				$("#question").append("What is " + num1 + " minus " + num2+ "? ");
-				question = "What is " + num1 + " minus " + num2+ "? ";
+				$("#question").text("What is " + num1 + " minus " + num2+ "? ");
+				question = num1 + " - " + num2+ " = ? ";
 				break;
 			case 3: //multiplication
 				answer = num1 * num2;
-				$("#question").append("What is " + num1 + " multiplied by " + num2+ "? ");
-				question = "What is " + num1 + " multiplied by " + num2+ "? ";
+				$("#question").text("What is " + num1 + " multiplied by " + num2+ "? ");
+				question = num1 + " x " + num2+ " = ? ";
 				break;
 			case 4: //division
 				answer = num1;
 				num1 *= num2;
 				answer = num1 / num2;
-				$("#question").append("What is " + num1 + " divided by " + num2+ "? ");
-				question = "What is " + num1 + " divided by " + num2+ "? ";
+				$("#question").text("What is " + num1 + " divided by " + num2+ "? ");
+				question = num1 + " / " + num2+ " = ? ";
 				break;
 		}
     }
@@ -69,6 +69,10 @@
 		if(input == answer){
 			$("#result").text("You are correct.");// + score);	
 			clearInterval(gameLoop);
+			context.clearRect(enemy1.x-5, enemy1.y-20, enemy1.width, enemy1.height);
+			enemy1 = new enemy (Math.floor(Math.random()*4)+1);
+			drawEnemy(enemy1, context);
+			QuestionMaker();
 		}else{
 			$("#result").text("You are wrong.");
 			}
@@ -76,55 +80,111 @@
 
 
 //Motion
-	//Animation
-      var gameLoop = setInterval(function(){animate(myObj, canvas, context)}, 1000/60);
+	  //Animation
+	  var bgInterval = null;
+	  var bgPosition = 0;
+      var gameLoop = setInterval(function(){
+		  bgInterval = window.setInterval(bgAnimation, 200);
+		  animate(enemy1, canvas, context);
+		  }, 1000/60);
       var canvas = document.getElementById('myCanvas');
       var context = canvas.getContext('2d');
-      var score = 10000;
-      var imageObj = new Image();
+	  canvas.width = window.innerWidth*0.8;
+	  canvas.height = window.innerHeight*0.8;
+	  var score = 10000;
+	  var lives = 3;
+	  var imageObj = new Image();
+	  
+	  var rectX = canvas.width/2-50;
+	  var rectY = canvas.height/2-50;
       imageObj.src = 'obj.png';
 
+	  var bgImg = new Image();
+	  bgImg.src = "space.jpg";
+	  //Background Animation
+	  function bgAnimation() {
+    	bgPosition--;
+    	$("canvas").css({backgroundPosition: (bgPosition * 1) + "px 0px"});
+		if(Math.abs(bgPosition)>=bgImg.width){
+			bgPosition = 0;
+			bgInterval = null;
+			bgInterval = window.setInterval(bgAnimation, 1000);
+			}
+		}
 
-    var myObj = {
-        x: 0,
-        y: 75,
-        width: 180,
-        height: 50,
-        borderWidth: 2
-      };
-      function drawRectangle(myObj, context) {
-		context.drawImage(imageObj, myObj.x, myObj.y);
+
+	  function enemy (side) {
+        if(side==2||side==4)
+		{
+			this.y= Math.floor(Math.random()*canvas.height)+1;
+			if(side==2)
+				this.x= canvas.width+50;
+			else
+				this.x=-50;
+		}
+		else if(side==1||side==3)
+		{
+			this.x= Math.floor(Math.random()*canvas.width)+1;
+			if(side==3)
+				this.y= canvas.height+50;
+			else
+				this.y=-50;
+		}
+       
+	
+		this.fixedX= this.x;
+		this.fixedY= this.y;
+        this.width= 100;
+        this.height= 125;
+        this.borderWidth=  2
+     }
+	  var enemy1 = new enemy (Math.floor(Math.random()*4)+1);
+      function drawEnemy(enemy1, context) {
+		context.drawImage(imageObj, enemy1.x, enemy1.y);
         
         //context.beginPath();
-        //context.rect(myObj.x, myObj.y, myObj.width, myObj.height);
-        //context.fillStyle = '#8ED6FF';
-        //context.fill();
-        context.lineWidth = myObj.borderWidth;
+        context.rect(rectX,rectY,50,50);
+        context.fillStyle = '#8ED6FF';
+        context.fill();
+        context.lineWidth = enemy1.borderWidth;
 		context.fillStyle = '#FFF';
 		context.font = 'bold 15px Calibri';
-        context.fillText(question, myObj.x, myObj.y - 5);
+      	context.fillText(question, enemy1.x, enemy1.y - 5);
         //context.strokeStyle = 'black';
         context.stroke();
       }
-      function animate(myObj, canvas, context) {
-        var newX = myObj.x + 2;
+      function animate(target, canvas, context) {
+        newX = (target.fixedX - rectX)/30000*60;
+		newY = (target.fixedY - rectY)/30000*60;
 		score = Math.round(score-1000/60);
-        if(newX < canvas.width - myObj.width - myObj.borderWidth / 2) {
-          myObj.x = newX;
-        }
-		else
+		
+        if(target.x > rectX||target.x<rectX-80) {
+          target.x -= newX;
+		 }
+		
+		 if(target.y > rectY-50||target.y<rectY){
+			target.y-= newY;
+		 }
+		 
+		// clear
+        context.clearRect(0, 0, canvas.width, canvas.height);
+		//draw again
+        drawEnemy(enemy1, context);
+        
+		if(target.x < rectX+80&&target.x>rectX-100&&target.y < rectY+80&&target.y>rectY-100)
 		{
 			clearInterval(gameLoop);
 			$("#result").text("You Lose.");
+			lives-=1;
+			context.clearRect(enemy1.x-5, enemy1.y-20, enemy1.width, enemy1.height);
+			enemy1 = new enemy (Math.floor(Math.random()*4)+1);
+			drawEnemy(enemy1, context);
+			QuestionMaker();
 		}
 
-        // clear
-        context.clearRect(0, 0, canvas.width, canvas.height);
 
-        drawRectangle(myObj, context);
 
       }		
-        drawRectangle(myObj, context);
+	      drawEnemy(enemy1, context);
 
 
-		
