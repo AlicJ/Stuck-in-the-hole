1274f6635908df25219c06276c00404d8da43d7f
Alic Jiang <zichen.jiang@yahoo.com>
2013-05-27 09:46:17 -0400

    credit

--- js/functions.js
+++ js/functions.js
@@ -16,7 +16,7 @@ function mainAnimation() {
 }
 function creditAnimation() {
     creditPosition--;
-	$("#credit").css({backgroundPositionY: (creditPosition) + "px"});
+	$("#credit").css({backgroundPosition: "20px " + (creditPosition) + "px"});
 }
 function splash() {
 	$('#splash').fadeOut("800");
